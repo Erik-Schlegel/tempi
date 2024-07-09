ba46cc083238af80c881a62ffec4868e24ddc5bd
Erik Schlegel <epschlegel@gmail.com>
2024-07-09 12:17:47 -0700

    Remove install file.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt update &&
-  sudo apt upgrade -y &&
-  CONFIG_LINE='usb_max_current_enable=1' &&
-  if ! grep -q "$CONFIG_LINE" /boot/firmware/config.txt; then echo "$CONFIG_LINE" | sudo tee --append /boot/firmware/config.txt >/dev/null; fi &&
-  echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf &&
-  echo 'Reboot to apply changes'
