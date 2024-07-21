4c6f140a3e170861c9b7b1e16acd4be0fd3d4d2b
Erik Schlegel <epschlegel@gmail.com>
2024-07-20 20:49:48 -0700

    Increase error logging.

--- tempi.py
+++ tempi.py
@@ -2,6 +2,17 @@ import subprocess
 import json
 from datetime import datetime
 from zoneinfo import ZoneInfo
+import logging
+import select
+
+# Configure logging to write to the mounted directory
+logging.basicConfig(filename="/tempi/log/error.log", level=logging.ERROR)
+
+
+def log_error(message):
+    logging.error(message)
+    print(message)
+
 
 MAX_DAILY_MESSAGES = 10
 
@@ -35,44 +46,34 @@ def update_day(data):
 
 
 def is_current_precedent_changed():
-    # print("in is_current_precedent_changed")
     channel_low = current_data.get(EXAMINED_CHANNELS_LOW_HIGH[0])
     channel_high = current_data.get(EXAMINED_CHANNELS_LOW_HIGH[1])
     if current_precedent is None or channel_low is None or channel_high is None:
-        # print("unknown precedent or not enough data to determine temperature precedent")
         return False
     else:
-        # print("temperature precedent changed")
         return current_precedent != (channel_low["Temp"] > channel_high["Temp"])
 
 
 def update_current_precedent(status="init"):
-    # print("in update_current_precedent")
-    # print("status", status)
-
     global current_precedent
-    # print("current_precedent before", current_precedent)
     channel_low = current_data.get(EXAMINED_CHANNELS_LOW_HIGH[0])
     channel_high = current_data.get(EXAMINED_CHANNELS_LOW_HIGH[1])
     if channel_low is None or channel_high is None:
         print("Cannot update precedent due to missing data")
         return
-
     current_precedent = channel_low["Temp"] > channel_high["Temp"]
 
 
 def main():
     previous_data_in = None
     message_count = 0
+    error_occurred = False
 
-    current_time = datetime.now(ZoneInfo("America/Los_Angeles")).strftime(
-        "%b %d, %y %H:%M:%S"
-    )
     subprocess.run(
         [
             "wget",
             "--post-data",
-            f"Tempi Started at { datetime.now(ZoneInfo('America/Los_Angeles')).strftime('%b %d, %y %H:%M:%S') }",
+            f"Tempi Started at {datetime.now(ZoneInfo('America/Los_Angeles')).strftime('%b %d, %y %H:%M:%S')}",
             "http://localhost/tempi",
             "-O",
             "-",
@@ -82,101 +83,112 @@ def main():
     process = subprocess.Popen(
         ["rtl_433", "-f", "915000000", "-F", "json"],
         stdout=subprocess.PIPE,
-        # stderr=subprocess.DEVNULL,  # Redirect stderr to suppress messages
+        stderr=subprocess.PIPE,
     )
 
-    print("\n")
+    while not error_occurred:
+        try:
+            ready_to_read_file_descriptors = select.select(
+                [process.stdout.fileno(), process.stderr.fileno()], [], []
+            )[0]
 
-    try:
-        # Indefinite loop reads input data from rtl_433
-        for line in iter(process.stdout.readline, b""):
-            # data: {
-            # 'time': '2024-06-27 13:27:14',
-            # 'model': 'AmbientWeather-WH31E',
-            # 'id': 43,
-            # 'channel': 2,
-            # 'battery_ok': 1,
-            # 'temperature_C': 24.4,
-            # 'humidity': 41,
-            # 'data': 'c100000000',
-            # 'mic': 'CRC'
-            # }
-            data = json.loads(line.decode("utf-8"))
+            if process.stderr.fileno() in ready_to_read_file_descriptors:
+                error_output = process.stderr.readline()
+                if error_output:
+                    log_error(f"Subprocess stderr: {error_output.decode('utf-8')}")
+                    error_occurred = True
+                    break  # Exit the loop if an error is detected
 
-            # The same data signal is sent 2x by each sensor.
-            if data == previous_data_in:
-                continue
-            previous_data_in = data
+            if process.stdout.fileno() in ready_to_read_file_descriptors:
+                line = process.stdout.readline()
+                if line:
+                    # data: {
+                    # 'time': '2024-06-27 13:27:14',
+                    # 'model': 'AmbientWeather-WH31E',
+                    # 'id': 43,
+                    # 'channel': 2,
+                    # 'battery_ok': 1,
+                    # 'temperature_C': 24.4,
+                    # 'humidity': 41,
+                    # 'data': 'c100000000',
+                    # 'mic': 'CRC'
+                    # }
+                    data = json.loads(line.decode("utf-8"))
 
-            # add/update data in current_data. Size constrained to the CHANNELS defined above.
-            # current_data: {
-            #   1: {'Room': 'Music Room', 'Temp': 79.0, 'H20': 40},
-            #   2: {'Room': 'Living Room', 'Temp': 79.0, 'H20': 40},
-            #   3: {'Room': 'Bedroom', 'Temp': 79.0, 'H20': 40},
-            #   4: {'Room': 'Garage', 'Temp': 83.3, 'H20': 36},
-            #   5: {'Room': 'Outside', 'Temp': 85.8, 'H20': 32},
-            # }
-            current_data[data["channel"]] = {
-                "Room": CHANNELS[data["channel"]],
-                "Temp": to_fahrenheit(data["temperature_C"]),
-                "H20": data["humidity"],
-            }
+                    if data == previous_data_in:
+                        continue
+                    previous_data_in = data
 
-            print("\033c", end="")  # clear the screen
-            for channel in CHANNELS:
-                if channel in current_data:
-                    print(
-                        f"{current_data[channel]['Room']}: {current_data[channel]['Temp']}F, {current_data[channel]['H20']}%"
-                    )
-                else:
-                    print(f"{CHANNELS[channel]}: No data")
+                    # add/update data in current_data. Size constrained to the CHANNELS defined above.
+                    # current_data: {
+                    #   1: {'Room': 'Music Room', 'Temp': 79.0, 'H20': 40},
+                    #   2: {'Room': 'Living Room', 'Temp': 79.0, 'H20': 40},
+                    #   3: {'Room': 'Bedroom', 'Temp': 79.0, 'H20': 40},
+                    #   4: {'Room': 'Garage', 'Temp': 83.3, 'H20': 36},
+                    #   5: {'Room': 'Outside', 'Temp': 85.8, 'H20': 32},
+                    # }
+                    current_data[data["channel"]] = {
+                        "Room": CHANNELS[data["channel"]],
+                        "Temp": to_fahrenheit(data["temperature_C"]),
+                        "H20": data["humidity"],
+                    }
 
-            print("\n")
+                    print("\033c", end="")  # clear the screen
+                    for channel in CHANNELS:
+                        if channel in current_data:
+                            print(
+                                f"{current_data[channel]['Room']}: {current_data[channel]['Temp']}F, {current_data[channel]['H20']}%"
+                            )
+                        else:
+                            print(f"{CHANNELS[channel]}: No data")
 
-            if is_new_day(data):
-                message_count = 0
-                update_day(data)
+                    print("\n")
 
-            if current_precedent is None:
-                update_current_precedent("initializing")
-                continue
+                    if is_new_day(data):
+                        message_count = 0
+                        update_day(data)
 
-            if is_current_precedent_changed():
-                update_current_precedent("actual")
-                if current_precedent and message_count < MAX_DAILY_MESSAGES:
-                    message = f"{CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[0]]} is {'warmer' if current_precedent else 'cooler'} than {CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[1]]}"
-                    subprocess.run(
-                        [
-                            "wget",
-                            "--post-data",
-                            "living room is warmer than outside",
-                            "http://localhost/tempi",
-                            "-O",
-                            "-",
-                        ]
-                    )
+                    if current_precedent is None:
+                        update_current_precedent("initializing")
+                        continue
 
-                    print(
-                        f"{CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[0]]} is warmer than {CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[1]]}"
-                    )
-                else:
-                    subprocess.run(
-                        [
-                            "wget",
-                            "--post-data",
-                            "living room is cooler than outside",
-                            "http://localhost/tempi",
-                            "-O",
-                            "-",
-                        ]
-                    )
-                    print(
-                        f"{CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[0]]} is cooler than {CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[1]]}"
-                    )
-                message_count += 1
+                    if is_current_precedent_changed():
+                        update_current_precedent("actual")
+                        if current_precedent and message_count < MAX_DAILY_MESSAGES:
+                            message = f"{CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[0]]} is {'warmer' if current_precedent else 'cooler'} than {CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[1]]}"
+                            subprocess.run(
+                                [
+                                    "wget",
+                                    "--post-data",
+                                    "living room is warmer than outside",
+                                    "http://localhost/tempi",
+                                    "-O",
+                                    "-",
+                                ]
+                            )
 
-    except Exception as ex:
-        print(ex)
+                            print(
+                                f"{CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[0]]} is warmer than {CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[1]]}"
+                            )
+                        else:
+                            subprocess.run(
+                                [
+                                    "wget",
+                                    "--post-data",
+                                    "living room is cooler than outside",
+                                    "http://localhost/tempi",
+                                    "-O",
+                                    "-",
+                                ]
+                            )
+                            print(
+                                f"{CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[0]]} is cooler than {CHANNELS[EXAMINED_CHANNELS_LOW_HIGH[1]]}"
+                            )
+                        message_count += 1
+
+        except Exception as ex:
+            log_error(f"Exception in main loop: {ex}")
+            error_occurred = True  # Set the error flag to true to exit the loop
 
 
 if __name__ == "__main__":
