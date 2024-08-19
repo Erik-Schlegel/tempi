38d4937b77bb69c594efef0a6e8cbcc57ab29e76
Erik Schlegel <epschlegel@gmail.com>
2024-08-19 00:05:08 -0700

    Refactor for Flask standards; support local ui iteration.

--- frontend.py
+++ frontend.py
@@ -1,33 +1,23 @@
 import os
-from flask import Flask, render_template_string, send_from_directory
-from jinja2 import Environment, FileSystemLoader
+from flask import Flask, render_template, send_from_directory
 
 api_url = os.getenv("API_URL")
 latitude = os.getenv("LATITUDE")
 longitude = os.getenv("LONGITUDE")
 time_zone_id = os.getenv("TIME_ZONE_ID")
 
-
 if api_url is None:
     raise ValueError("API_URL environment variable must be set")
 
 app = Flask(__name__)
-
-env = Environment(loader=FileSystemLoader("frontend"))
-template = env.get_template("index.html")
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route("/")
 def index():
-    rendered_html = template.render(API_URL=api_url, LATITUDE=latitude, LONGITUDE=longitude, TIME_ZONE_ID=time_zone_id)
-    return render_template_string(rendered_html)
+    return render_template("index.html", API_URL=api_url, LATITUDE=latitude, LONGITUDE=longitude, TIME_ZONE_ID=time_zone_id)
 
 
-@app.route('/client/<path:filename>')
+@app.route('/static/client/<path:filename>')
 def serve_client_files(filename):
-    return send_from_directory('frontend/client', filename)
-
-
-@app.route('/icons/<path:filename>')
-def serve_icon_files(filename):
-    return send_from_directory('frontend/icons', filename)
+    return send_from_directory('static/client', filename)
